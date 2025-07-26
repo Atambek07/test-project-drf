3d8ab660bd9a317f6f85fa3b90877274e4c64bd4
atambek <atambekbrat@gmail.com>
2025-07-26 23:54:51 +0600

    add some models

--- core/products/models.py
+++ core/products/models.py
@@ -8,6 +8,16 @@ class Product(models.Model):
 
     preview_image = models.ImageField(null=True, blank=True, verbose_name='Product Preview Image')
 
+    bonus_count = models.IntegerField(verbose_name='Product Bonus Count', blank=True, null=True)
+    old_price = models.IntegerField(verbose_name='Product Old Price', blank=True, null=True)
+    full_description = models.TextField(verbose_name='Product Full Description', max_length=200)
+    is_on_sale = models.BooleanField(verbose_name='Product On Sale', default=False)
+
+    weight = models.FloatField(verbose_name='Product Weight', default=0.0)
+    country_production = models.CharField(verbose_name='Products production country')
+    brend = models.CharField(verbose_name='Product Brend', max_length=200, blank=True, null=True)
+
+
     class Meta:
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
