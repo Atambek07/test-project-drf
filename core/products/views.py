a2ac550c6431a6c800aae4b431dd8b97398d5b57
Atambek <117630074+Atambek07@users.noreply.github.com>
2025-07-27 12:20:48 +0600

    Update views.py
    Add in views registration population

--- core/products/views.py
+++ core/products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . import serializers, models
+from rest_framework import generics
+
+class RegPopView(generics.ListAPIView):
+  queryset = models.Register_Population.objects.all()
+  serializer_class = serializers.RegPopSerializer
